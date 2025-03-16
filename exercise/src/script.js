914bf254a1104ccb5b5db775074a434ed7f736c9
Vương Linh <Linh.VuongBao@Student.HTW-Berlin.de>
2025-03-16 22:00:46 +0700

    messing around with request animation frame method

--- exercise/src/script.js
+++ exercise/src/script.js
@@ -29,3 +29,18 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+//Animations
+const tick = () => 
+{
+    
+    //Update objects 
+    mesh.rotation.y += 0.01
+
+    //Renderer 
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
